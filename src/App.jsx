3a9caf41d9074c654588b08b941f28fb78002874
ruhinim <malithmahettiarachchi@gmail.com>
2025-05-13 19:10:30 +0530

    Project Initiation

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './App.css'
+import Navbar from './Components/Navbar'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Hello World</h1>
+      <h2>This is a test</h2>
+    </>
+  )
+}
+
+export default App
